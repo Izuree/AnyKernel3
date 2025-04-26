209b4c4cc936244efe3a3a60848a2489c520a611
kvsnr113 <kvsnrprojkt113@gmail.com>
2025-04-26 20:53:13 +0700

    update

--- anykernel.sh
+++ anykernel.sh
@@ -1,6 +1,12 @@
 # AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
+
+
 # E404 kernel custom installer by 113
+
+# What'supp kangers ? looking for what i am cooking ?
+# Anything you do or if you are taking a peek and going to use it
+# PUT A FUCKING PROPER CREDITS !!!
 
 properties() { '
 kernel.string=E404 Kernel by Project 113
@@ -38,80 +44,94 @@ fi;
 set_perm_recursive 0 0 750 750 $ramdisk/*;
 
 ui_print " ";
-ui_print "Kernel Naming :";
+ui_print "-> Kernel Naming :";
 ui_print "--> "$ZIPFILE" ";
 ui_print " ";
 
 case "$ZIPFILE" in
-  *miui*|*MIUI*)
-    ui_print "MIUI/HyperOS DTBO variant detected,";
-    ui_print "--> Using MIUI/HyperOS DTBO... ";
-    mv *-miui-dtbo.img $home/dtbo.img;
-    rm *-normal-dtbo.img;
+  *noksu*|*NOKSU*)
+    ui_print "-> Non-KernelSU variant selected,";
+    E404_KSU=0;
   ;;
-  *aosp*|*AOSP*)
-    ui_print "Normal DTBO variant detected,";
-    ui_print "--> Using Normal DTBO... ";
-    mv *-normal-dtbo.img $home/dtbo.img;
-    rm *-miui-dtbo.img;
+  *ksu*|*KSU*)
+    ui_print "-> KernelSU variant selected,";
+    E404_KSU=1;
   ;;
   *)
-    ui_print "DTBO is not specified !!!";
-    ui_print "--> Using Normal DTBO... ";
-    mv *-normal-dtbo.img $home/dtbo.img;
-    rm *-miui-dtbo.img;
+    ui_print "-> KernelSU is not specified !!!";
+    E404_KSU=0;
   ;;
 esac
+ui_print "--> Patching KernelSU cmdline...";
+mv *-Image $home/Image;
+rm *-Image;
 ui_print " ";
 
 case "$ZIPFILE" in
-  *noksu*|*NOKSU*)
-    ui_print "Non-KernelSU variant detected,";
-    ui_print "--> Using Non-KernelSU Kernel Image...";
-    mv *-noksu-Image $home/Image;
-    rm *-ksu-Image;
+  *miui*|*MIUI*|*hyper*|*HYPER*)
+    ui_print "-> MIUI/HyperOS variant selected,";
+    ui_print "--> Patching MIUI/HyperOS kernel cmdline...";
+    E404_ROM_TYPE=2;
   ;;
-  *ksu*|*KSU*)
-    ui_print "KernelSU variant detected,";
-    ui_print "--> Using KernelSU Kernel Image...";
-    mv *-ksu-Image $home/Image;
-    rm *-noksu-Image;
+  *aosp*|*AOSP*|*clo*|*CLO*)
+    ui_print "-> AOSP/CLO variant selected,";
+    ui_print "--> Patching AOSP/CLO kernel cmdline... ";
+    E404_ROM_TYPE=1;
   ;;
   *)
-    ui_print "KernelSU is not specified !!!";
-    ui_print "--> Using Non-KernelSU Kernel Image...";
-    mv *-noksu-Image $home/Image;
-    rm *-ksu-Image;
+    ui_print "-> ROM is not specified !!!";
+    ui_print "--> Patching default kernel cmdline... ";
+    E404_ROM_TYPE=0;
   ;;
 esac
 ui_print " ";
 
 case "$ZIPFILE" in
   *effcpu*|*EFFCPU*)
-    ui_print "Efficient CPUFreq variant detected,";
-    ui_print "--> Using Efficient CPUFreq DTB...";
-    mv *-effcpu-dtb $home/dtb;
-    rm *-normal-dtb;
+    ui_print "-> Efficient CPUFreq variant selected,";
+    ui_print "--> Patching efficient CPUFreq cmdline...";
+    E404_EFFCPU=1;
   ;;
   *)
-    ui_print "Normal CPUFreq variant detected,";
-    ui_print "--> Using Normal CPUFreq DTB...";
-    mv *-normal-dtb $home/dtb;
-    rm *-effcpu-dtb;
+    ui_print "-> Normal CPUFreq variant selected,";
+    ui_print "--> Patching normal CPUFreq cmdline...";
+    E404_EFFCPU=0;
   ;;
 esac
 
-if [ ! -f /vendor/etc/task_profiles.json ]; then
+if [ ! -f /vendor/etc/task_profiles.json ] && [ ! -f /system/vendor/etc/task_profiles.json ]; then
   ui_print " ";
-	ui_print "Your rom does not have Uclamp task profiles,";
-	ui_print "Please install Uclamp task profiles module !";
+	ui_print "-> Your rom does not have Uclamp task profiles !";
+	ui_print "-> Please install Uclamp task profiles module !";
   ui_print "--> Ignore this if you already have.";
 fi;
+
+mv *-dtbo.img $home/dtbo.img;
+mv *-dtb $home/dtb;
 
 ## AnyKernel install
 dump_boot;
 
 # Begin Ramdisk Changes
+if [ "$E404_KSU" == "1" ]; then
+  patch_cmdline "e404_kernelsu" "e404_kernelsu=1";
+else
+  patch_cmdline "e404_kernelsu" "e404_kernelsu=0";
+fi
+
+if [ "$E404_ROM_TYPE" == "2" ]; then
+  patch_cmdline "e404_rom_type" "e404_rom_type=2";
+elif [ "$E404_ROM_TYPE" == "1" ]; then
+  patch_cmdline "e404_rom_type" "e404_rom_type=1";
+else
+  patch_cmdline "e404_rom_type" "e404_rom_type=0";
+fi
+
+if [ "$E404_EFFCPU" == "1" ]; then
+  patch_cmdline "e404_effcpu" "e404_effcpu=1";
+else
+  patch_cmdline "e404_effcpu" "e404_effcpu=0";
+fi
 
 # migrate from /overlay to /overlay.d to enable SAR Magisk
 if [ -d $ramdisk/overlay ]; then
